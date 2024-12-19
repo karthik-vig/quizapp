8f88673ae40d7fc396e4157e6eafe38359f5c5d6
Karthik Vignesh Sundar <karthik.v@swirepay.com>
2024-12-19 17:52:33 +0530

    initial commit

---
+++ src/main/java/com/kvs/app/quizapp/controller/QuizAppController.java
@@ -0,0 +1,33 @@
+package com.kvs.app.quizapp.controller;
+
+import org.springframework.http.HttpStatus;
+// import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+// import org.springframework.web.bind.annotation.RequestParam;
+import jakarta.servlet.http.HttpSession;
+
+import com.kvs.app.quizapp.dto.RequestBodies.Login;
+
+
+@RestController
+@RequestMapping("/api")
+public class QuizAppController {
+
+    @PostMapping("/login")
+    public ResponseEntity<?> startLogin(@RequestBody Login login) {
+        if (login.getEmail() == "karthik.v@swirepay.com") {
+            return new ResponseEntity<>("Success", HttpStatus.OK);
+        } else {
+            System.out.println(login.getEmail());
+            return new ResponseEntity<>("Failed", HttpStatus.UNAUTHORIZED);
+        }
+        // TestDTO testDto = new TestDTO("123", "karthik.v@swirepay.com");
+        // return ResponseEntity.ok(testDto);
+    }
+}
---
+++ src/main/java/com/kvs/app/quizapp/dto/RequestBodies/Login.java
@@ -0,0 +1,13 @@
+package com.kvs.app.quizapp.dto.RequestBodies;
+
+public class Login {
+    private String email;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
