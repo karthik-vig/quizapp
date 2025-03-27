ff243a9476a97f11f718ec1302905e88f4ff2d12
karthik-vig <s.karthik6789+github_prof@gmail.com>
2025-03-27 12:33:31 +0530

    modified the database to use ULID instead of UUIDv4 and also created foreign keys and indexes for efficient peformance

--- setup_database.sql
+++ setup_database.sql
@@ -1,59 +1,77 @@
+use quiz_app;
+
 create table if not exists emailotp (
-    id varchar(40) primary key,
+    id char(26) primary key,
     email varchar(255) not null,
     otp int not null,
     createdat timestamp default current_timestamp
 );
 
 create table if not exists users (
-    id varchar(40) primary key,
+    id char(26) primary key,
     username text,
     email text not null,
     createdat timestamp default current_timestamp,
     updatedat timestamp on update current_timestamp
 );
 
+create table if not exists contacts (
+    id char(26) primary key,
+    userid char(26) not null,
+    relateduserid char(26),
+    createdat timestamp default current_timestamp,
+    updatedat timestamp on update current_timestamp,
+    foreign key (userid) references users(id),
+    foreign key (relateduserid) references users(id),
+    index userid_index (userid),
+    index relateduserid_index (relateduserid)
+);
+
 create table if not exists quiztemplate (
-    id varchar(40) primary key,
-    userid varchar(40) not null,
+    id char(26) primary key,
+    userid char(26) not null,
     quiztemplatetitle varchar(255) not null,
     quiztemplate longtext not null,
     createdat timestamp default current_timestamp,
-    updatedat timestamp on update current_timestamp
-);
-
-create table if not exists submissions (
-    id varchar(40) primary key,
-    userid varchar(40) not null,
-    quizid varchar(40) not null,
-    quizanswers longtext not null,
-    createdat timestamp default current_timestamp,
-    updatedat timestamp on update current_timestamp
+    updatedat timestamp on update current_timestamp,
+    foreign key (userid) references users(id),
+    index userid_index (userid)
 );
 
 create table if not exists quizzes (
-    id varchar(40) primary key,
-    userid varchar(40) not null,
+    id char(26) primary key,
+    userid char(26) not null,
     quiztitle text not null,
     quizdata longtext not null,
     createdat timestamp default current_timestamp,
     updatedat timestamp on update current_timestamp,
-    expiration timestamp
+    expiration timestamp,
+    foreign key (userid) references users(id),
+    index userid_index (userid)
+);
+
+create table if not exists submissions (
+    id char(26) primary key,
+    userid char(26) not null,
+    quizid char(26) not null,
+    quizanswers longtext not null,
+    createdat timestamp default current_timestamp,
+    updatedat timestamp on update current_timestamp,
+    foreign key (userid) references users(id),
+    foreign key (quizid) references quizzes(id),
+    index userid_index (userid),
+    index quizid_index (quizid)
 );
 
 create table if not exists invites (
-    id varchar(40) primary key,
-    userid varchar(40) not null,
-    quizid varchar(40) not null,
+    id char(26) primary key,
+    userid char(26) not null,
+    quizid char(26) not null,
     invitestatus boolean not null,
     createdat timestamp default current_timestamp,
-    updatedat timestamp on update current_timestamp
-);
-
-create table if not exists contacts (
-    id varchar(40) primary key,
-    userid varchar(40) not null,
-    relateduserid varchar(40),
-    createdat timestamp default current_timestamp,
-    updatedat timestamp on update current_timestamp
+    updatedat timestamp on update current_timestamp,
+    foreign key (userid) references users(id),
+    foreign key (quizid) references quizzes(id),
+    index userid_index (userid),
+    index quizid_index (quizid)
 );
